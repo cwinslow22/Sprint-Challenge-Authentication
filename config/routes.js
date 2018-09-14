8d6e46fdae506d1c13671e767a57fa2e437ff881
Charlie Winslow <cwinslow22@yahoo.com>
2018-09-14 11:26:23 -0500

    completed the register and login endpoints
    able to return list of jokes from api/jokes
    
    MVP complete

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,12 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const db = require('../database/dbConfig.js');
 
 const { authenticate } = require('./middlewares');
+
+const secret = require('../_secrets/keys').jwtKey;
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -8,12 +14,58 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+function conjureToken(user) {
+  const payload = {
+      username: user.username,
+  };
+  const options = {
+      expiresIn: '2h',
+      jwtid: '222', // jti
+      subject: `${user.id}`,
+  };
+  return jwt.sign(payload, secret, options);
+}
+
 function register(req, res) {
   // implement user registration
+  const creds = req.body;
+
+  const hash = bcrypt.hashSync(creds.password, 12);
+  
+  creds.password = hash;
+  db('users')
+      .insert(creds)
+      .then(ids => {
+          const id = ids[0];
+
+          db('users')
+              .where({ id })
+              .first()
+              .then(user => {
+                  const token = conjureToken(user);
+                  res.status(201).json({ id: user.id, token });
+              })
+              .catch(err => res.status(500).send(err));
+      })
+      .catch(err => res.status(500).send(err));
 }
 
 function login(req, res) {
   // implement user login
+  const creds = req.body;
+
+  db('users')
+      .where({ username: creds.username })
+      .first()
+      .then(user => {
+          if (user && bcrypt.compareSync(creds.password, user.password)) {
+              const token = conjureToken(user);
+              res.status(200).json({ token });
+          } else {
+              res.status(401).json({ message: 'Nice try bub' });
+          }
+      })
+      .catch(err => res.status(500).send(err));
 }
 
 function getJokes(req, res) {
