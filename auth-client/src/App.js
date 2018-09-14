0b64f9d1038830dd4ecc2159f467cf798a2fc44c
Charlie Winslow <cwinslow22@yahoo.com>
2018-09-14 12:34:54 -0500

    built out sign in and jokes components

--- auth-client/src/App.js
+++ auth-client/src/App.js
@@ -3,17 +3,21 @@ import logo from './logo.svg';
 import {  Route, withRouter } from "react-router-dom";
 import './App.css';
 
+import SignUp from './components/signUp';
+import SignIn from './components/signIn';
+import Jokes from './components/jokes';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to the future</h1>
+          <h1 className="App-title">Welcome to your future</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route path="/signup" component={SignUp} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/users" component={Jokes} />
       </div>
     );
   }
---
+++ auth-client/src/components/jokes.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Jokes extends Component {
+  state = {
+    jokes: [],
+  };
+
+  render() {
+    return (
+      <div>
+          <ul>
+              {this.state.users.map(user => (
+              <li key={user.id}>{user.username} from {user.department}</li>
+              ))}
+          </ul>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem('jwt');
+    const reqOptions = {
+        headers: {
+            Authorization: token,
+        },
+    };
+
+    axios
+        .get('http://localhost:3300/api/jokes', reqOptions)
+        .then(res => {
+            console.log('users data:', res.data);
+            this.setState({ users: res.data.users }); // or this.setState({ users: res.data }) if using res.json( users ); in the server index file for the get/api/users endpoint
+        })
+        .catch(err => {
+            console.error('Axios Failed', err ); // res.response.data
+            this.props.history.push('/signin');
+        });
+    // console.log('state', this.state);
+  };
+}
+
+export default Jokes;
